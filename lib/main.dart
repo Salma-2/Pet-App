3bbf60d64b57d9ee4a9a777ba2c73a0a30cb562e
Mostafico <mostafaibrahimragab@gmail.com>
2021-04-11 17:36:47 +0200

    Added chat screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pet_app/presentation/screens/chat_screen.dart';
 import 'package:pet_app/presentation/screens/home_screen.dart';
 import 'package:pet_app/presentation/theme/theme.dart';
 import 'presentation/screens/on_boarding.dart';
@@ -13,7 +15,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Pet Care',
-      theme: AppTheme.lightTheme,
+      theme: ThemeData(
+        // This changes font for the entire app using the Google Fonts package
+        // from pub.dev : https://pub.dev/packages/google_fonts
+        textTheme: GoogleFonts.nunitoSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        // You can change theme colors to directly change colors for the whole
+        // app.
+        primaryColor: Color(0xff5B428F),
+        accentColor: Color(0xffF56D58),
+        primaryColorDark: Color(0xff262833),
+        primaryColorLight: Color(0xffFCF9F5),
+      ),
+      darkTheme: ThemeData(
+        // This changes font for the entire app using the Google Fonts package
+        // from pub.dev : https://pub.dev/packages/google_fonts
+        textTheme: GoogleFonts.nunitoSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        // You can change theme colors to directly change colors for the whole
+        // app.
+        primaryColor: Color(0xff5B428F),
+        accentColor: Color(0xffF56D58),
+        primaryColorDark: Color(0xffFFFFFF),
+        primaryColorLight: Color(0xff000000),
+      ),
       home: OnBoardingPage(),
     );
   }
