b7a1ce75cacec1707c507a31f4024016624cb5ae
salma <salmahamdisaad@gmail.com>
2021-04-11 16:27:59 +0200

    fixed merge bugs

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Pet Care',
       theme: AppTheme.lightTheme,
-      home: HomeScreen(),
+      home: OnBoardingPage(),
     );
   }
 }
